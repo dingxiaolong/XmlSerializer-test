b9daf2e66304cd6e486870145c097136d1a7a33c
dingxiaolong <xiaolong881115@126.com>
2023-03-21 16:43:24 +0800

    Add files via upload

---
+++ testXMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace testXMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ testXMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace testXMaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
